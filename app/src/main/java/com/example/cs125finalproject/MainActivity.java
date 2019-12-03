b71eb12e0e67a6cee410539e9b27b8f38edba225
Michelle <liangmichelles@gmail.com>
2019-12-02 22:07:39 -0600

    UI Edits

--- app/src/main/java/com/example/cs125finalproject/MainActivity.java
+++ app/src/main/java/com/example/cs125finalproject/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.cs125finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Ringtone;
+import android.media.RingtoneManager;
 import android.os.Bundle;
 import android.widget.TextClock;
 import android.widget.TimePicker;
@@ -18,13 +20,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         alarm = findViewById(R.id.timePicker);
         currentTime = findViewById(R.id.textClock);
+        final Ringtone ring = RingtoneManager.getRingtone(getApplicationContext(), RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
         Timer time = new Timer();
         time.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
                 if (currentTime.getText().toString().equals(AlarmToTime())) {
+                    ring.play();
                     //WEB API FOR SPOTIFY
                     //CURRENT RINGTONE WILL PLAY
+                } else {
+                    ring.stop();
                 }
             }
         }, 0, 1000);
